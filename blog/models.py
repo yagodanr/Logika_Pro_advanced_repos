b509299676d4cace09a92c8c30bc2a214ab622ee
yagodanr <yagodanr@gmail.com>
2024-02-03 13:28:10 +0200

    I don't why Post() doesn't work.$ e:/Programming/Logika_Extra/django/django_orm/Django_orm_logika_blog/venv/Scripts/python.exe e:/Programming/Logika_Extra/django/django_orm/Django_orm_logika_blog/main.py Traceback (most recent call last):   File e:ProgrammingLogika_Extradjangodjango_ormDjango_orm_logika_blogmain.py, line 69, in <module>     Post(   File E:ProgrammingLogika_Extradjangodjango_ormDjango_orm_logika_blogvenvLibsite-packagesdjangodbmodelsbase.py, line 567, in __init__     raise TypeError( TypeError: Post() got unexpected keyword arguments: 'title'

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
     
@@ -13,9 +14,11 @@ class User(models.Model):
     }
     role = models.CharField(max_length=100, choices=ROLE_CHOICES, default="user")
 
+    def __str__(self) -> str:
+        return self.login
+
 class TimeStampMixin(models.Model): #https://stackoverflow.com/questions/3429878/automatic-creation-date-for-django-model-form-objects
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
 
     class Meta:
         abstract = True
@@ -24,10 +27,10 @@ class TimeStampMixin(models.Model): #https://stackoverflow.com/questions/3429878
 class Post(TimeStampMixin):
     title = models.CharField(max_length=127),
     text = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
-class Content(TimeStampMixin):
+class Comment(TimeStampMixin):
     text = models.TextField()
-    post = models.ForeignKey(Post)
-    author = models.ForeignKey(User)
+    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     
