4f68cf9ca1c004470049a37d06210c91f28454e2
yagodanr <yagodanr@gmail.com>
2024-02-09 12:47:57 +0200

    Added model Post and made migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date =models.DateTimeField(default=timezone.now, editable=False)
+
