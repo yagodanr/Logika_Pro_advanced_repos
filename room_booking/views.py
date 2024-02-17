90eae8c3086221dab22fad738b7a49d3c1f82856
yagodanr <yagodanr@gmail.com>
2024-02-17 19:06:11 +0200

    added rooms_list pages

--- room_booking/views.py
+++ room_booking/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def get_rooms_list(request):
+    rooms = Room.objects.all()
+    
+    context = {
+        "rooms" : rooms
+    }
+    
+    return render(
+        request,
+        "room_booking/rooms_list.html",
+        context
+    )
+
