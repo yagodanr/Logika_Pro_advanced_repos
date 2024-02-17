a5c2018bcd7e8363de5d171a767dad99fc0d068d
yagodanr <yagodanr@gmail.com>
2024-02-17 18:32:13 +0200

    Fixed setting.py

--- hotel/settings.py
+++ hotel/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "room_booking.app.RoomBookingConfig"
+    "room_booking.apps.RoomBookingConfig"
 ]
 
 MIDDLEWARE = [
