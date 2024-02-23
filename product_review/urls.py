4a1c38e09b26367a2cd96cd361da90660c32b3b7
yagodanr <yagodanr@gmail.com>
2024-02-23 23:11:18 +0200

    Added views for Products

--- product_review/urls.py
+++ product_review/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import *
+
+
 urlpatterns = [
+    path("", get_products, name="products"),
+    path("product/<int:prod_id>", get_product_info, name="prod_info"),
     
 ]
--- product_review/views.py
+++ product_review/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
+
+from .models import *
+
 # Create your views here.
+
+def base(request):
+    return render(request, "base.html")
+
+
+def get_products(request):
+    products = Product.objects.all()
+    
+    context = {
+        "products": products
+    }
+    
+    return render(
+        request,
+        "product_review/products.html",
+        context
+    )
+    
+def get_product_info(request, prod_id: int):
+    try: 
+        product = Product.objects.get(id=prod_id)
+    except:
+        raise 
+    
+    
+    context = {
+        "product": product
+    }
+    
+    return render(
+        request, 
+        "product_review/prod_info.html",
+        context
+    )
+    
+
