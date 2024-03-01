0a1306f5045b9966fc3a6c215dbbc418df29608c
yagodanr <yagodanr@gmail.com>
2024-03-01 23:20:37 +0200

    Added Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+
+    username = models.CharField(max_length=255)
+    text =  models.TextField()
+    date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = ("Review")
+        verbose_name_plural = ("Reviews")
+
+    def __str__(self):
+        return self.name
+
+
